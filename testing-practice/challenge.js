9d94496fa13b418ea20a6465935544a48208e642
DANIEL WILSON <daniel.j.wilson@btinternet.com>
2024-01-25 09:59:32 +0000

    started palindrome

--- testing-practice/challenge.js
+++ testing-practice/challenge.js
@@ -17,6 +17,10 @@ export function divide(a, b) {
 // these are much much harder - it's a huge stretch goal, don't worry about getting these done.
 export function isPalindrome() {
   // implement function
+  for (let i = str.length - 1; i >= 0; i--) {
+    reversedString += str[i];
+  }
+  console.log(reversedString); // same as reversedString = reversedString + str[i];
 }
 
 export function isAnagram() {
